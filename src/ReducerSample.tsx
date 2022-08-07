761a698b7da57710ad80b6b93fb9abb5f2d3cda7
1eedaegon <d8726243@gmail.com>
2022-08-07 18:30:03 +0900

    Update sample reducer to context API

--- src/ReducerSample.tsx
+++ src/ReducerSample.tsx
@@ -1,73 +1,37 @@
-import { useReducer } from 'react';
-
-type Color = 'red' | 'orange' | 'yellow';
-
-type State = {
-  count: number;
-  text: string;
-  color: Color;
-  isGood: boolean;
-};
-
-type Action =
-  | { type: 'SET_COUNT'; count: number }
-  | { type: 'SET_TEXT'; text: string }
-  | { type: 'SET_COLOR'; color: Color }
-  | { type: 'TOGGLE_ISGOOD' };
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case 'SET_COUNT':
-      return { ...state, count: action.count };
-    case 'SET_TEXT':
-      return { ...state, text: action.text };
-    case 'SET_COLOR':
-      return { ...state, color: action.color };
-    case 'TOGGLE_ISGOOD':
-      return { ...state, isGood: !state.isGood };
-    default:
-      throw new Error('Unhandled action');
-  }
-}
+import { useSampleDispatch, useSampleState } from './SampleContext';
 
 function ReducerSample() {
-  const [state, dispatch] = useReducer(reducer, {
-    count: 0,
-    text: 'hello',
-    color: 'red',
-    isGood: true,
-  });
-  const setCountToFive = () => dispatch({ type: 'SET_COUNT', count: 5 });
-  const setTextToBye = () => dispatch({ type: 'SET_TEXT', text: 'bye' });
-  const setColorToYellow = () =>
-    dispatch({ type: 'SET_COLOR', color: 'yellow' });
-  const toggleGood = () => dispatch({ type: 'TOGGLE_ISGOOD' });
+  const state = useSampleState();
+  const dispatch = useSampleDispatch();
+  const increaseCount = () =>
+    dispatch({ type: 'SET_COUNT', count: state.count + 1 });
+  const decreaseCount = () =>
+    dispatch({ type: 'SET_COUNT', count: state.count - 1 });
+  const setText = () => dispatch({ type: 'SET_TEXT', text: 'Confirmed' });
+  const setColor = () => dispatch({ type: 'SET_COLOR', color: 'blud' });
+  const toggleIsGood = () => dispatch({ type: 'TOGGLE_ISGOOD' });
   return (
     <div>
       <p>
-        <code>count: </code>
-        {state.count}
+        <code>count: </code> {state.count}
       </p>
       <p>
-        <code>text: </code>
-        {state.text}
+        <code>text: </code> {state.text}
       </p>
       <p>
-        <code>color: </code>
-        {state.color}
+        <code>color: </code> {state.color}
       </p>
       <p>
-        <code>isGood: </code>
-        {state.isGood ? 'true' : 'false'}
+        <code>Is good?: </code> {state.isGood ? 'good' : 'not good'}
       </p>
       <div>
-        <button onClick={setCountToFive}>SET_COUNT -&gt 5</button>
-        <button onClick={setTextToBye}>SET_TEXT -&gt bye</button>
-        <button onClick={setColorToYellow}>SET_COLOR -&gt yellow</button>
-        <button onClick={toggleGood}>TOGGLE_GOOD -&gt true or false</button>
+        <button onClick={increaseCount}>Increase cnt</button>
+        <button onClick={decreaseCount}>Increase cnt</button>
+        <button onClick={setText}>Set confirmed</button>
+        <button onClick={setColor}>Change to blue</button>
+        <button onClick={toggleIsGood}>Toggle is good</button>
       </div>
     </div>
   );
 }
-
 export default ReducerSample;
